375865aff469de7686704d91af364fdbd9cc50ed
LAPTOP-1UIQE60B\OperThirteen <oprthrtn@gmail.com>
2022-06-07 21:37:48 +0700

    Установлен Redux.

---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,12 @@
+const initialState = {
+   
+};
+
+export function bilingoReducer(state = initialState, action) {
+    let newState = {...state};
+    switch(action.type) {
+        
+        default: 
+            return newState;
+    }
+}
