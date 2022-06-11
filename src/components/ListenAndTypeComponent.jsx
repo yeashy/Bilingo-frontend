23a685c3c7fcf12bc096259a1690c3326acf78d6
yeashy <gertyn07@gmail.com>
2022-06-11 11:07:39 +0300

    added footer, changed cound icon

--- src/components/ListenAndTypeComponent.jsx
+++ src/components/ListenAndTypeComponent.jsx
@@ -1,11 +1,11 @@
 import {Flex, Text,Input} from "@chakra-ui/react";
-import {BiMicrophone} from 'react-icons/bi'
+import {AiOutlineSound} from 'react-icons/ai'
 
 function ListenAndTypeComponent() {
     return (
         <Flex flexDirection='column' bgColor='white' borderRadius='16px' p='16px' mt='15px' align='center'>
             <Text fontSize='2xl'>Listen and type!</Text>
-            <Text  fontSize='3xl' my='10px'><BiMicrophone/></Text>
+            <Text  fontSize='3xl' my='10px'><AiOutlineSound/></Text>
             <Input/>
         </Flex>
     )
